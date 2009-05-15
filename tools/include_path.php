776bfb596a06ea2ae72fb2fd8e1df8b718d83e88
Richard Pope <richard@memespring.co.uk>
2009-05-15 09:51:31 +0000

    basic rotate tool

---
+++ tools/include_path.php
@@ -0,0 +1,15 @@
+<?php
+	//get config file
+
+ 	require_once(dirname(__FILE__) . "../../config/general.php");
+
+	//set include path
+	$include_path = ".:";
+	$include_path .= INCLUDE_DIR . ":";
+	$include_path .= CONFIG_DIR . ":";
+	$include_path .= INCLUDE_DIR . "/PEAR:";
+
+	//$include_path .= get_include_path();
+
+	set_include_path($include_path);
+?>
---
+++ tools/rotate.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+    rotate an image
+*/
+
+ 	require_once(dirname(__FILE__) . "/include_path.php");
+	require_once(dirname(__FILE__) . "/../includes/init.php");
+
+    //specific user_id set?
+	$swiches = getopt('i:d:l:');
+	$leaflet_image_id = isset($swiches['i']) ? $swiches['i'] : null;	
+	$degrees = isset($swiches['d']) ? $swiches['d'] : 0;
+	$leaflet_id = isset($swiches['l']) ? $swiches['l'] : null;		
+    		
+	//setup search
+	$search = factory::create('search');
+    $leaflet_images = array();
+	//all images vs 1
+	if(isset($leaflet_image_id)){
+    	//get a single image
+    	$leaflet_images = $search->search('leaflet_image', 
+    		array(array("leaflet_image_id", "=", $leaflet_image_id)));	    
+    }else{
+        //get all images for a leaflet
+    	$leaflet_images = $search->search('leaflet_image', 
+    		array(array("leaflet_id", "=", $leaflet_id)));            		
+    }
+
+	foreach ($leaflet_images as $leaflet_image) {
+        $leaflet_image->rotate($degrees);
+	}
+
+?>
