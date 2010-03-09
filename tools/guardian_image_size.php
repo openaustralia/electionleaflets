5a7fb0983b2e6f9d2b98283ab5a6fc977573d1e0
Richard Pope <richard@memespring.co.uk>
2010-03-09 10:21:02 +0000

    image resize debugging

--- tools/guardian_image_size.php
+++ tools/guardian_image_size.php
@@ -11,8 +11,10 @@
         print "resizing" . "\n";
         $file_name = IMAGES_DIR . "/original/" . $leaflet_image->image_key . ".jpg";	        
         if (file_exists($file_name)){
-            resize_image($file_name, IMAGE_SMALL_SIZE, IMAGES_DIR . "/small/" . $file_id . ".jpg");
-            resize_image($file_name, IMAGE_THUMBNAIL_SIZE, IMAGES_DIR . "/thumbnail/" . $file_id . ".jpg", true);
+            print "new image " . IMAGES_DIR . "/small/" . $leaflet_image->image_key . ".jpg \n"
+            print IMAGE_SMALL_SIZE . '\n'
+            resize_image($file_name, IMAGE_SMALL_SIZE, IMAGES_DIR . "/small/" . $leaflet_image->image_key . ".jpg");
+            resize_image($file_name, IMAGE_THUMBNAIL_SIZE, IMAGES_DIR . "/thumbnail/" . $leaflet_image->image_key . ".jpg", true);
             print "resized" . "\n";            
         }else{
             print "failed" . "\n";
