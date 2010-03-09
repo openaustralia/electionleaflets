e7850c3edce8d2c0e8d3086c74556f8d27885cb4
Richard Pope <richard@memespring.co.uk>
2010-03-09 10:14:08 +0000

    image resize debugging

--- tools/guardian_image_size.php
+++ tools/guardian_image_size.php
@@ -8,12 +8,14 @@
     $leaflet_images = $search->search("leaflet_image", array(array("1", "=", "1")));
     
     foreach ($leaflet_images as $leaflet_image) {
+        print "resizing" . "\n";
         $file_name = IMAGES_DIR . "/original/" . $leaflet_image->image_key . ".jpg";	        
         if (file_exists($file_name)){
             resize_image($file_name, IMAGE_SMALL_SIZE, IMAGES_DIR . "/small/" . $file_id . ".jpg");
             resize_image($file_name, IMAGE_THUMBNAIL_SIZE, IMAGES_DIR . "/thumbnail/" . $file_id . ".jpg", true);
+            print "resized" . "\n";            
         }else{
-            $return = false;
+            print "failed" . "\n";
         }
     }
 
