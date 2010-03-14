4eea773ee5ffafe82a8d2e3e8ceaa864212afd3e
Richard Pope <richard@memespring.co.uk>
2010-03-14 14:37:07 +0000

    fix tags

--- tools/fix_tags.php
+++ tools/fix_tags.php
@@ -10,13 +10,22 @@ $tag = factory::create('tag');
 $sql = "select count(tag_id) as count, trim(tag) as clean_tag, min(tag_id) as min, max(tag_id) as max from tag group by trim(tag) having count(tag_id) > 1";
 
 $tags = $tag->execute($sql);
-
+/*
 foreach ($tags as $tag) {
     $search = factory::create('search');
     $leaflet_tags = $search->search('leaflet_tag', array(array("tag_id", "=", $tag->max)));
     foreach ($leaflet_tags as $leaflet_tag) {
         $leaflet_tag->tag_id = $tag->min;
         $leaflet_tag->update();
+    }
+}
+*/
+
+foreach ($tags as $tag) {
+    $search = factory::create('search');
+    $delete_tags = $search->search('tag', array(array("tag_id", "=", $tag->max)));
+    foreach ($delete_tags as $delete_tag) {
+        $delete_tag->delete();
     }
 }
 
