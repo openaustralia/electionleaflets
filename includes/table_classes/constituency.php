9f6fc11cf14c0873f2dda1be763fc00997dd3d39
Richard Pope <richard@memespring.co.uk>
2010-04-14 15:34:31 +0000

    count fix

--- includes/table_classes/constituency.php
+++ includes/table_classes/constituency.php
@@ -84,7 +84,7 @@ class tableclass_constituency extends tablebase {
     public static function constituency_count($limit = 10, $cache = true){
         $return = array();
 	    $constituency = factory::create('constituency');
-	    $sql = "select count(leaflet_constituency.leaflet_constituency_id) as count, constituency.name, constituency.constituency_id from constituency inner join leaflet_constituency on constituency.constituency_id = leaflet_constituency.constituency_id group by constituency.name, constituency.constituency_id limit " . $limit;
+	    $sql = "select count(leaflet_constituency.leaflet_constituency_id) as count, constituency.name, constituency.constituency_id from constituency inner join leaflet_constituency on constituency.constituency_id = leaflet_constituency.constituency_id group by constituency.name, constituency.constituency_id order by count(leaflet_constituency.leaflet_constituency_id) desc limit " . $limit;
 	    if($cache){
             $return = $constituency->execute_cached($sql);
         }else{
