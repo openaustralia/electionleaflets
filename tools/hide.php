7471aecbea604585f9224ea835f8b651b95473eb
Richard Pope <richard@memespring.co.uk>
2010-04-15 13:54:06 +0000

    hide

--- tools/hide.php
+++ tools/hide.php
@@ -3,12 +3,12 @@
     require_once(dirname(__FILE__) . "/include_path.php");
 	require_once(dirname(__FILE__) . "/../includes/init.php");
 
-	$swiches = getopt('l:s');
+	$swiches = getopt('i:s');
 	$leaflet_id = isset($swiches['i']) ? $swiches['i'] : null;	
 	$show = isset($swiches['s']) ? 1 : 0;
 
     if(!isset($leaflet_id)){
-        print "help: php hide.php -i[LEALFET ID] -s[show]";
+        print "help: php hide.php -i[LEALFET ID] -s[show] \n";
     }else{
      	$search = factory::create('search');
      	$leaflets = $search->search('leaflet', 
