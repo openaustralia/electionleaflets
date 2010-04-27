011821e205f69f14c255fc98170c4171572f5832
Richard Pope <richard@memespring.co.uk>
2010-04-27 15:16:59 +0000

    rss rewrite

--- docs/leaflets.php
+++ docs/leaflets.php
@@ -66,8 +66,8 @@ class leaflets_page extends pagebase {
             $this->leaflet_search->start = null;
             $this->leaflet_search->number = null;
             $all_leaflets = $this->leaflet_search->search(true);
-
             $total_count = count($all_leaflets);
+
         }else{
             $leaflet = factory::create('leaflet');
             $total_count = $leaflet->count_cached();   
