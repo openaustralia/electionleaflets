275dbd62b2a30108ebb0faf48fc5da507a0faf27
Matt Joyce <matt.joyce@gmail.com>
2010-07-28 10:53:21 +1000

    EL-115 total was including non-live leaflets

--- docs/leaflets.php
+++ docs/leaflets.php
@@ -65,14 +65,10 @@ class leaflets_page extends pagebase {
             
             $this->leaflet_search->start = null;
             $this->leaflet_search->number = null;
-            $all_leaflets = $this->leaflet_search->search(true);
-            $total_count = count($all_leaflets);
-
-        }else{
-            $leaflet = factory::create('leaflet');
-            $total_count = $leaflet->count_cached();   
         }
-        
+
+        $all_leaflets = $this->leaflet_search->search(true);
+        $total_count = count($all_leaflets);    
         $total_pages = ceil($total_count / PAGE_ITEMS_COUNT);        
         
         
