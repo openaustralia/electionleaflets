f28c1ded12dc1db64d7d988a21b208fa931902a5
Tim 'mithro' Ansell <mithro@mithis.com>
2011-01-29 17:48:51 +1000

    Fixed EL-177 - Tags showing up for wrong election.

--- includes/table_classes/tag.php
+++ includes/table_classes/tag.php
@@ -35,11 +35,13 @@ class tableclass_tag extends tablebase {
     }
 
 
-    public function get_weighted_tags($limit = 100){
+    public function get_weighted_tags($limit = 100, $election_id = null){
+        $election_id = get_election_id($election_id);
 	$sql ="SELECT tag.tag, tag.tag_id, COUNT(leaflet_tag.leaflet_id) AS count FROM tag ";
 	$sql.="INNER JOIN leaflet_tag ON tag.tag_id = leaflet_tag.tag_id ";
 	$sql.="INNER JOIN leaflet ON leaflet_tag.leaflet_id=leaflet.leaflet_id ";
-	$sql.="WHERE leaflet.live=1 ";
+	$sql.="INNER JOIN leaflet_election ON leaflet.leaflet_id=leaflet_election.leaflet_id ";
+	$sql.="WHERE leaflet.live=1 AND leaflet_election.election_id=$election_id ";
 	$sql.="GROUP BY tag.tag, tag.tag_id ORDER BY COUNT(leaflet_tag.leaflet_id) DESC LIMIT " . $limit;
         return $this->execute($sql);
     }
