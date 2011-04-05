f09341df81bc76116edbba9000d8a20c50807e6c
Henare Degan <henare.degan@gmail.com>
2011-04-05 13:37:48 +1000

    Tidy up code formatting only

--- docs/categories.php
+++ docs/categories.php
@@ -2,27 +2,22 @@
 require_once('init.php');
 
 class categories_page extends pagebase {
+  // Bind
+  function bind() {
+    $this->page_title = "Election leaflets by category";
+    $search = factory::create('search');
 
-	//bind
-	function bind() {
-		$this->page_title = "Election leaflets by category";
-
-		$search = factory::create('search');
-
-		//get top parties
-        $categories = $search->search_cached("category", 
-                array(array("1", "=", "1")),
-                "AND",
-                null,
-                array(array("name", "ASC"))
-            );
-        $this->assign("categories", $categories);
-	}
-	
-	
+    // Get categories
+    $categories = $search->search_cached("category",
+      array(array("1", "=", "1")),
+      "AND",
+      null,
+      array(array("name", "ASC"))
+    );
+    $this->assign("categories", $categories);
+  }
 }
 
-//create class instance
+// Create class instance
 $categories_page = new categories_page();
-
 ?>
