6603b5fc02b7e9aeaef19be529fe158bd90d0d3c
Henare Degan <henare.degan@gmail.com>
2013-01-15 13:42:21 +1100

    Now we can edit the election name

--- docs/admin/edit_election.php
+++ docs/admin/edit_election.php
@@ -3,30 +3,56 @@ require_once('init.php');
 
 class edit_election_page extends pagebase {
 
-    function setup(){
+    function load(){
         $election_id = get_http_var('id');
         if(!isset($election_id)){
             throw_404();
         }else{
             $this->election_id = $election_id;
+
+            $search = factory::create('search');
+            $result = $search->search("election",
+                array(array("election_id", "=", $this->election_id))
+            );
+            $election_details = null;
+            if(count($result) != 1){
+                throw_404();
+            }else{
+                $this->election_details = $result[0];
+            }
         }
     }
 
     function bind() {
         $this->page_title = "Edit";
 
-        $search = factory::create('search');
-        $result = $search->search("election",
-            array(array("election_id", "=", $this->election_id))
-        );
-        $election_details = null;
-        if(count($result) != 1){
-            throw_404();
+        $this->assign("election_details", $this->election_details);
+    }
+
+    function process() {
+        if($this->validate()){
+            $this->election_details->name = trim($this->data['txtName']);
+
+            if($this->election_details->update()){
+                $this->bind();
+                $this->render();
+            }else{
+                trigger_error("Unable to save election");
+            }
         }else{
-            $election_details = $result[0];
+            $this->bind();
+            $this->render();
+        }
+    }
+
+    function validate()
+    {
+        if(!isset($this->data['txtName']) || $this->data['txtName'] ==''){
+            $this->add_warning('Please add a name for this election');
+            $this->add_warn_control('txtName');
         }
 
-        $this->assign("election_details", $election_details);
+        return count($this->warnings) == 0;
     }
 }
 
--- templates/admin/editelection.tpl
+++ templates/admin/editelection.tpl
@@ -3,6 +3,21 @@
 <div class="contentfull align-left">
     <h1>Edit the {$election_details->name}</h1>
     <p><a href="/admin">« Admin home</a></p>
+
+    <form id="frmUpload" method="post" action="{$form_action}">
+        {include file="../formvars.tpl"}
+        <fieldset>
+            <ul>
+                <li>
+                    <label for="txtName">Election Name *</label>
+                    <input type="text" id="txtName" name="txtName" {if $warn_txtName}class="error"{/if} value="{$election_details->name}"/>
+                </li>
+            </ul>
+        </fieldset>
+        <div class="buttons">
+            <input type="submit"  value="Update election"/>
+        </div>
+    </form>
 </div>
 
 {include file="../footer.tpl"}
