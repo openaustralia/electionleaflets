f636d0b1fb3e6f186dda8699bc7213e180ce76ef
Matthew Landauer <matthew@openaustralia.org>
2013-08-26 10:11:53 +1000

    Make sure that the synced files on S3 are readable by everyone

--- s3syncimages.sh
+++ s3syncimages.sh
@@ -1,4 +1,4 @@
-s3cmd sync data/images/small/  s3://electionleafletsaustralia/small/ 
-s3cmd sync data/images/medium/  s3://electionleafletsaustralia/medium/
-s3cmd sync data/images/large/  s3://electionleafletsaustralia/large/
-s3cmd sync data/images/thumbnail/  s3://electionleafletsaustralia/thumbnail/
+s3cmd sync -v -P data/images/small/  s3://electionleafletsaustralia/small/ 
+s3cmd sync -v -P data/images/medium/  s3://electionleafletsaustralia/medium/
+s3cmd sync -v -P data/images/large/  s3://electionleafletsaustralia/large/
+s3cmd sync -v -P data/images/thumbnail/  s3://electionleafletsaustralia/thumbnail/
